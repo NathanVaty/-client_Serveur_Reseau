bc75e2db790848935217164bf6f79e15519168e3
NathanVaty <nvaty@etud.univ-jfc.fr>
2018-12-05 20:16:48 +0100

    reglage indentation

--- src/client_serveur/Serveur.java
+++ src/client_serveur/Serveur.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package client_serveur;
 
 import java.io.BufferedReader;
@@ -23,11 +23,11 @@ public class Serveur {
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         
-                BufferedReader fluxEntree;
-                PrintWriter fluxSortie; 
-                BufferedReader stdOut = new BufferedReader(new InputStreamReader(
-                                           System.in));
-            
+        BufferedReader fluxEntree;
+        PrintWriter fluxSortie;
+        BufferedReader stdOut = new BufferedReader(new InputStreamReader(
+                System.in));
+        
         try {
             serverSocket = new ServerSocket(4444);
         } catch (IOException e) {
@@ -38,73 +38,33 @@ public class Serveur {
         try {
             boolean running = true;
             clientSocket = serverSocket.accept();
-                String hostName = clientSocket.getInetAddress().getHostName();
-                 fluxSortie = new PrintWriter(clientSocket.getOutputStream(),true);
-                 fluxEntree = new BufferedReader(new InputStreamReader(
-                                            clientSocket.getInputStream()));
-                System.out.println("Connexion de: " + hostName);
-                 String servInput;
-                 String messageEntrant;
-                
+            String hostName = clientSocket.getInetAddress().getHostName();
+            fluxSortie = new PrintWriter(clientSocket.getOutputStream(),true);
+            fluxEntree = new BufferedReader(new InputStreamReader(
+                    clientSocket.getInputStream()));
+            System.out.println("Connexion de: " + hostName);
+            String servInput;
+            String messageEntrant;
+            
             while(running){
                 messageEntrant = fluxEntree.readLine();
                 System.out.println("from: " + hostName + ": " + messageEntrant);
                 if(messageEntrant.equals("bye")) {
-                     running = false;
+                    running = false;
                 } else {
-                     servInput = stdOut.readLine();
+                    servInput = stdOut.readLine();
                     fluxSortie.println(servInput);
                     if (servInput.equals("bye")) {
-                             running = false;           
-                         }
-                }   
+                        running = false;
+                    }
+                }
             }
-             clientSocket.close();
-             serverSocket.close();
+            clientSocket.close();
+            serverSocket.close();
         } catch(IOException e) {
             System.out.println("Accept failed on port 4444");
             System.exit(-1);
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
