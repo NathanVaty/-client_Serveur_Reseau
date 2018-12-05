d06bf87a7a5a8542642fd2d1df96ad8732553361
NathanVaty <nvaty@etud.univ-jfc.fr>
2018-12-05 17:44:34 +0100

    Début serveur, reçoit la réponse et répond

--- src/client_serveur/Serveur.java
+++ src/client_serveur/Serveur.java
@@ -5,11 +5,72 @@
  */
 package client_serveur;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author Nathan Vaty
  */
 public class Serveur {
-    
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        
+                BufferedReader fluxEntree = new BufferedReader(new InputStreamReader(
+                                            clientSocket.getInputStream()));
+                PrintWriter fluxSortie = new PrintWriter(clientSocket.getOutputStream(),true);
+                BufferedReader stdOut = new BufferedReader(new InputStreamReader(
+                                           System.in));
+            
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.out.println();
+            System.exit(-1);
+        }
+        
+        try {
+            while(true){
+                clientSocket = serverSocket.accept();
+                String hostName = clientSocket.getInetAddress().getHostName();
+                System.out.println(hostName);
+                System.out.println("from: " + hostName + ": " + fluxEntree.readLine());
+                String servInput;
+                while ((servInput = stdOut.readLine()) != null) {
+                    fluxSortie.println(servInput);
+                    
+                }
+            }
+            
+        } catch(IOException e) {
+            System.out.println("Accept failed on port 4444");
+            System.exit(-1);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
